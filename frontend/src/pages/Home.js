96eba9954246932f6d437f1de02670ee07cc4de8
emresen20 <emrebernarn@gmail.com>
2024-12-04 01:17:23 +0300

    concection with backend and frontend

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 
 export default function Home() {
-  return (
-    <div className='home'>
-        <h2>Anasayfa</h2>
-    </div>
-  )
+    const [notlar, setNotlar] = useState([]);
+
+    useEffect(() => {
+        const fetchNotlar = async () => {
+            try {
+                const response = await fetch('/api/notlar'); // burası önemli packetjsona proxi ekledi
+               
+                const json = await response.json();
+                console.log(json)
+                if (response.ok) {
+                    setNotlar(json);
+                } else {
+                    console.error('Error fetching notes:', json);
+                }
+            } catch (error) {
+                console.error('Error fetching notes:', error);
+            }
+        };
+
+        fetchNotlar();
+    }, []);
+
+    return (
+        <div className="home">
+            <div className="notlar">
+                {notlar.map((not) => (
+                    <p key={not._id}>{not.baslik}</p>
+                ))}
+            </div>
+        </div>
+    );
 }
