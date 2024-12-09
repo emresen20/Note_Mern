36dcd550fb41532e1fe8ef377623cf44ef22b821
emresen20 <emrebernarn@gmail.com>
2024-12-09 21:06:21 +0300

    hasing with bcyrp

--- backend/models/kullaniciModel.js
+++ backend/models/kullaniciModel.js
@@ -1,4 +1,5 @@
 const mongoose=require('mongoose')
+const bcrypt=require('bcrypt')
 
 const Sema=mongoose.Schema
 
@@ -16,5 +17,23 @@ const kullaniciSema= new Sema({
         required:true, 
        }
 })
+
+
+kullaniciSema.static.signup=async function(email,parola){
+
+    const kontrolKullanici=await this.findOne({email}) // burada this kullaniciSemayı temsil eder
+    
+    if(kontrolKullanici){
+        throw Error('Email zaten kullanılıyor')
+    }
+
+    const salt= await bcrypt.genSalt(10)
+
+    const sifrelenmisParola= await bcrypt.hash(parola,salt)
+
+    const kullanici=await this.create({email,parola:sifrelenmisParola})
+
+    return kullanici
+}
 
 module.exports=mongoose.model('Kullanici',kullaniciSema)
