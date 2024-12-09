5d830694eefa1b7115faa14be3551c87926a7376
emresen20 <emrebernarn@gmail.com>
2024-12-09 21:53:00 +0300

    token created

--- backend/controllers/kullaniciController.js
+++ backend/controllers/kullaniciController.js
@@ -1,4 +1,11 @@
 const Kullanici= require('../models/kullaniciModel')
+const jwt= require('jsonwebtoken')
+
+
+const tokenOlustur=(_id)=>{
+    return jwt.sign({_id},process.env.SECRET_KEY,{expiresIn:'1h'})
+}
+
 
 const loginKullanıcı= async(req,res)=>{
     res.json({mesaj:'Login işlemi gerçekleştirildi'})
@@ -9,7 +16,8 @@ const signupKullanıcı= async(req,res)=>{
 
     try {
         const kullanici= await Kullanici.signup(email,parola)
-        res.status(200).json({email,kullanici})
+        const token=tokenOlustur(kullanici._id);
+        res.status(200).json({email,token})
         
     } catch (error) {
         res.status(400).json({hata:error.message})
